888552dab21db1a7c000386380fe2b034e64578a
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-02-03 12:59:59 +0100

    fix: main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 
-import { ThemeProvider as LegacyThemeProvider } from "@saleor/macaw-ui";
 import { ThemeProvider } from "@saleor/macaw-ui/next";
 import "@saleor/macaw-ui/next/style";
 
@@ -9,11 +8,9 @@ import { App } from "./App";
 
 render(
   <React.StrictMode>
-    <LegacyThemeProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </LegacyThemeProvider>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root") as HTMLElement
 );
